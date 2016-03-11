39dda4e4ea0f39db98d3fd615c94b0dcf9992f6c
Sleptsov Denis <sleptsov.denis@gmail.com>
2016-03-11 14:44:40 +0200

    js tasks module added

---
+++ js/js/tasks/task01.js
@@ -0,0 +1,6 @@
+function pow(number, degree) {
+    number = prompt("Enter number: ", "0");
+    degree = prompt("Enter degree: ", "0");
+    var result;
+    console.log("Number " + number + " in degree " + degree + " is " + result);
+}
