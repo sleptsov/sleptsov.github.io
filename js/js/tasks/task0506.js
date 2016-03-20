4751cb5db1c5dd166f71dc7fae0c07d279e76ef9
Sleptsov Denis <sleptsov.denis@gmail.com>
2016-03-20 15:48:57 +0200

    task0506 fix

--- js/js/tasks/task0506.js
+++ js/js/tasks/task0506.js
@@ -7,7 +7,25 @@ var counter = 1;
 
 var watch = new Stopwatch(timer);
 
-startPauseBtn.addEventListener('click', function(){
+startPauseBtn.addEventListener('click', watchStartPause);
+
+resetBtn.addEventListener('click', watchReset);
+
+circleBtn.addEventListener('click', watchGetCircle);
+
+window.addEventListener("keydown", function(e){
+    if (e.keyCode == 83) {
+        watchStartPause();
+    }
+    if (e.keyCode == 82) {
+        watchReset();
+    }
+    if(e.keyCode === 67) {
+        watchGetCircle();
+    }
+});
+
+function watchStartPause(){
     if (watch.isOn) {
         watch.stop();
         startPauseBtn.innerHTML = 'Resume';
@@ -19,18 +37,18 @@ startPauseBtn.addEventListener('click', function(){
         circleBtn.classList.remove('disabled');
         timer.classList.remove('timer-scale-down');
     }
-});
+}
 
-resetBtn.addEventListener('click', function () {
+function watchReset(){
     watch.stop();
     watch.reset();
     circleBtn.classList.add('disabled');
     startPauseBtn.innerHTML = 'Start';
-});
+}
 
-circleBtn.addEventListener('click', function () {
+function watchGetCircle() {
     watch.getCircle();
-});
+}
 
 function Stopwatch(elem) {
 
@@ -131,97 +149,3 @@ function Stopwatch(elem) {
         circle();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* FIRST VERSION*/
-
-/*
-var time = 50000;
-var running = false;
-var timer;
-var hours, mins, secs, ms;
-var counter = 1;
-
-function startPause(){
-    if (!running){
-        running = true;
-        increment();
-        document.getElementById('startPause').innerHTML = 'Pause';
-        document.getElementById('circle-btn').classList.remove('disabled');
-    } else {
-        running = false;
-        document.getElementById('startPause').innerHTML = 'Resume';
-        document.getElementById('circle-btn').classList.add('disabled');
-    }
-}
-
-function reset() {
-    clearInterval(timer);
-    time = hours = mins = secs = ms = 0;
-    running = false;
-    document.getElementById('circle-btn').classList.add('disabled');
-    document.getElementById('startPause').innerHTML = 'Start';
-    document.getElementById('timer').innerHTML = '00:00:00.0';
-    var circleBox = document.getElementById('circle-box');
-    while (circleBox.hasChildNodes()) {
-        circleBox.removeChild(circleBox.firstChild);
-    }
-    counter = 1;
-}
-
-function increment() {
-    if (running) {
-        timer = setTimeout(function() {
-            time++;
-            hours = addZero(Math.floor(time/10/60/60), 2);
-            mins = addZero(Math.floor(time/10/60), 2);
-            secs = addZero(Math.floor(time/10), 2);
-            ms = addZero((time%10), 0);
-            document.getElementById('timer').innerHTML = hours + ':' + mins + ':' + secs + '.' + ms;
-            increment();
-        }, 100)
-    }
-}
-
-function circle() {
-    if (running) {
-        var circleBox = document.getElementById('circle-box');
-        var circle = document.createElement('h4');
-        circle.innerHTML = 'circle ' + counter + ' - ' + hours + ':' + mins + ':' + secs + '.' + ms;
-        circleBox.appendChild(circle);
-        counter++;
-    }
-}
-
-function addZero(x,n) {
-    while (x.toString().length < n) {
-        x = "0" + x;
-    }
-    return x;
-}
-*/
