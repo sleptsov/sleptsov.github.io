b8bee6eaa796fe9f64241996a88868617928585b
Sleptsov Denis <sleptsov.denis@gmail.com>
2016-05-07 22:12:50 +0300

    riffsy api add

--- js/js/tasks/task15.js
+++ js/js/tasks/task15.js
@@ -1,3 +1,41 @@
 $(function(){
+    var url;
+    var $riffsyResults = $("#riffsy-results");
 
+    $('#riffsy-search').on('keypress', function(e){
+       if(e.keyCode == 13){
+           searchRiffsyGif();
+       }
+    });
+
+    $("#riffsy-btn").on('click', function(){
+       searchRiffsyGif();
+    });
+
+    function searchRiffsyGif(){
+        url = $("#riffsy-search").val();
+        $.ajax({
+            method: "GET",
+            url: "http://api.riffsy.com/v1/search?tag=" + url + "&limit=10",
+            dataType: "json",
+            success: function(data){
+               //console.log(data.results);
+                $.each(data.results, function(i, riff){
+                   // console.log(riff.url);
+                    $.each(riff.media, function(i, newRiff){
+                       //console.log(newRiff.nanogif.preview);
+
+                        var $riffsyLink = $('<a href='+ riff.url + ' target="_blank" ' + '></a>');
+                        var $riffsyImg = $('<img '+ 'src=' + newRiff.nanogif.url + ' class="img-thumbnail riffsy-img" ' + '>');
+                        $riffsyLink.append($riffsyImg);
+                        $riffsyResults.append($riffsyLink);
+                    })
+                })
+            },
+            error: function(){
+                console.log('Error');
+            }
+        });
+    }
 });
+
