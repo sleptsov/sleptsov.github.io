497ab00060d9a879a7c5db5a5dc0372efe6ffadb
Denis Sleptsov <sleptsov.denis@gmail.com>
2016-05-30 10:09:24 +0300

    search fix

--- gulp-sass/app/js/picload.js
+++ gulp-sass/app/js/picload.js
@@ -20,23 +20,29 @@ $(function(){
             url: "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(url),
             dataType: 'json'
         }).done(function(data){
-            var arraySliceToSeven = _.slice(data.hits, 0, 7);
-            _.forEach(arraySliceToSeven, function(item){
-                results.push(_.pick(item, ['webformatURL', 'tags']));
-            });
-            // Grab the template script
-            var theTemplateScript = $("#address-template").html();
-            // Compile the template
-            var theTemplate = Handlebars.compile(theTemplateScript);
-            // Define our data object
-            var context = {
-                data: results
-            };
-            // Pass our data to the template
-            var theCompiledHtml = theTemplate(context);
-            // Add the compiled html to the page
-            $('.content-placeholder').html(theCompiledHtml);
-
+            //console.log(data);
+            if (data.totalHits > 0){
+                var arraySliceToSeven = _.slice(data.hits, 0, 7);
+                _.forEach(arraySliceToSeven, function(item){
+                    results.push(_.pick(item, ['webformatURL', 'tags']));
+                });
+                // Grab the template script
+                var theTemplateScript = $("#address-template").html();
+                // Compile the template
+                var theTemplate = Handlebars.compile(theTemplateScript);
+                // Define our data object
+                var context = {
+                    data: results
+                };
+                // Pass our data to the template
+                var theCompiledHtml = theTemplate(context);
+                // Add the compiled html to the page
+                $('.content-placeholder').html(theCompiledHtml);
+                $('#search-message').html('Discover holiday activity ideas');
+            } else {
+                $('#search-message').html('No hits for '+ url +' found. Try again!');
+                $('.content-placeholder').html('');
+            }
         });
     }
     picLoad();
