ad0485c7dc9921acd390baa9765c1c7d53f4fc09
Denis Sleptsov <sleptsov.denis@gmail.com>
2016-06-14 13:35:49 +0300

    sass task fix

--- sass/app/js/accordion.js
+++ sass/app/js/accordion.js
@@ -1,3 +1,11 @@
 $(function(){
 
+    $('.accordion__item').on('click', '.accordion-link:not(.active)', function(e){
+        e.preventDefault();
+        $('.accordion-link').removeClass('active');
+        $('.accordion-panel').slideUp(400).removeClass('show');
+        $(this).addClass('active');
+        $(this).next('.accordion-panel').slideDown(400).addClass('show');
+    });
+
 });
